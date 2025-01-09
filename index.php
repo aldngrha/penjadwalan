a36029da336ecab1b310ce18697f377a55054efb
Aldi Nugraha <aldi.nugrahatk@gmail.com>
2025-01-09 15:06:41 +0700

    first init

---
+++ index.php
@@ -0,0 +1,146 @@
+<?php
+session_start();
+include 'koneksi.php';
+
+if (!isset($_SESSION["jabatan"])) {
+    echo "<script>location='login/index.php'</script>";
+    exit();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Poli Klinik | Dashboard</title>
+    <link href="assets/css/styles.css" rel="stylesheet" />
+    <link href="assets/css/dataTables.bootstrap4.min.css" rel="stylesheet" />
+    <script src="assets/js/all.min.js"></script>
+</head>
+
+<body class="sb-nav-fixed">
+    <?php include 'includes/navbar.php'; ?>
+
+    <div id="layoutSidenav">
+        <?php include 'includes/sidebar.php'; ?>
+        <div id="layoutSidenav_content" class="bg-white text-dark">
+            <main>
+                <div class="container-fluid">
+                    <h1 class="mt-4">Dashboard</h1>
+                    <ol class="breadcrumb mb-4">
+                        <li class="breadcrumb-item active">Dashboard</li>
+                    </ol>
+                    <!-- Content Row -->
+                    <div class="row">
+                        <!-- Earnings (Monthly) Card Example -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-primary h-100 py-2 bg-danger">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-white text-uppercase mb-1">
+                                                Data Jadwal</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray">
+                                                <?php $ambil = mysqli_query($koneksi, "SELECT * FROM tb_pasien"); ?>
+                                                <?php $count = mysqli_num_rows($ambil); ?>
+                                                <?php echo $count; ?>
+                                            </div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-calendar fa-2x text-gray"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Earnings (Monthly) Card Example -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-success h-100 py-2 bg-success">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-white text-uppercase mb-1">
+                                                Data Kelas</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray">
+                                                <?php $ambil = mysqli_query($koneksi, "SELECT * FROM tb_obat"); ?>
+                                                <?php $count = mysqli_num_rows($ambil); ?>
+                                                <?php echo $count; ?>
+                                            </div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-building fa-2x text-gray"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Earnings (Monthly) Card Example -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-info h-100 py-2 bg-info">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-white text-uppercase mb-1">Mata Pelajaran
+                                            </div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray">
+                                                <?php $ambil = mysqli_query($koneksi, "SELECT * FROM tb_pendaftaran WHERE status = '0'"); ?>
+                                                <?php $count = mysqli_num_rows($ambil); ?>
+                                                <?php echo $count; ?>
+                                            </div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-clipboard-list fa-2x text-gray"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Pending Requests Card Example -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-warning h-100 py-2 bg-warning">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-white text-uppercase mb-1">
+                                                Kategori</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray">
+                                                <?php $ambil = mysqli_query($koneksi, "SELECT * FROM tb_resep WHERE status_rsp = '0'"); ?>
+                                                <?php $count = mysqli_num_rows($ambil); ?>
+                                                <?php echo $count; ?>
+                                            </div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-list fa-2x gray"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </main>
+            <?php include 'includes/footer.php'; ?>
+        </div>
+    </div>
+    <script src="assets/js/jquery-3.5.1.slim.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/scripts.js"></script>
+    <script src="assets/js/Chart.min.js"></script>
+    <script src="assets/demo/chart-area-demo.js"></script>
+    <script src="assets/demo/chart-bar-demo.js"></script>
+    <script src="assets/js/jquery.dataTables.min.js"></script>
+    <script src="assets/js/dataTables.bootstrap4.min.js"></script>
+    <script src="assets/demo/datatables-demo.js"></script>
+</body>
+
+</html>
